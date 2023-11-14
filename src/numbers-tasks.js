e86f3258dfe89cf4f3ec823d05dcdd7dfdd24fcf
Adil <yesssng@gmail.com>
2023-11-14 22:36:52 +0600

    feat: 7 pass

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -159,7 +159,7 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(a, b, c) {
+function getParallelepipedDiagonal(/*a, b, c*/) {
   throw new Error('Not implemented');
 }
 
